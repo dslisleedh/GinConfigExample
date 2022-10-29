187851104cb5cdfa119dc9c6dd6181b6046da51b
dslisleedh <gnova6kud@gmail.com>
2022-10-29 21:39:39 +0900

    remove flip funcs from preprocessing

--- utils.py
+++ utils.py
@@ -35,8 +35,6 @@ def preprocessing(x, y, augment: bool = False):
     x = tf.cast(x, tf.float32) / 255.
     if augment:
         b, _, _, _ = x.get_shape().as_list()
-        x = tf.image.random_flip_left_right(x)
-        x = tf.image.random_flip_up_down(x)
         x = tf.image.random_crop(x, [b, 26, 26, 1])
         x = tf.image.resize(x, [28, 28])
     return x, y
